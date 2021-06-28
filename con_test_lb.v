500fbd750e99694f908933a824a19e438bce64d5
khk1262 <khk1262@naver.com>
2021-06-28 14:47:25 +0900

    linebuffer con complete

--- con_test_lb.v
+++ con_test_lb.v
@@ -1,4 +1,4 @@
-module con_test_lb(rst, clk, din, i_en, result, done);
+module con_test_lb(rst, clk, din, i_en, o_en, result, done);
 	input clk, rst;
 	reg wen, csn;
 
@@ -7,13 +7,14 @@ module con_test_lb(rst, clk, din, i_en, result, done);
 	input i_en;
 
 	output reg done;
+	output reg o_en;
 
 	wire [15:0] dout; 
 	output reg[18:0] result; 
 
 
 	reg[18:0] temp_sum;
-	wire o_r_en;
+	wire c_r_en;
 	
 	reg[10:0] din_ad;
 
@@ -24,6 +25,7 @@ module con_test_lb(rst, clk, din, i_en, result, done);
 	reg[3:0] count;
 	reg state; 
 	reg w_state;
+	reg[2:0] f_cnt;
 
 	wire[10:0] ad;
 
@@ -36,43 +38,43 @@ module con_test_lb(rst, clk, din, i_en, result, done);
 	reg [10:0] r_ptr_temp;	
 	reg [10:0] r_cnt; // rPtr cnt
 	reg [10:0] t_cnt; // total cnt
-	//reg [11:0] r_ad;
+
 	
 	reg [2:0] row;
 	reg [2:0] col;
 
 	assign ad = (count == 4'b0000) ? w_ptr : r_ptr_temp;
-	assign o_r_en = (state == 1 && count == 4'b1001) ? 1'b1 : 1'b0;
+	assign c_r_en = (state == 1 && count == 4'b1010) ? 1'b1 : 1'b0;
 
 	always@(*) begin
-		if(count == 0) sum = 0;
+		if(count == 4'b0001) sum = 0;
 		else sum = sum_flip;
-		if(count == 0) temp_sum = 0;
+		if(count == 4'b0001) temp_sum = 0;
 		else temp_sum = (dout>>mul);
 		temp_flip = temp_sum + sum;
 	end
 
 	always@(count) begin
-		if(count != 4'b1011) begin
-		if(count == 1 || count == 3 || count == 7 || count == 9)
-			mul <= HX;
-		else if(count == 2 || count == 4 || count == 6 || count == 8)
-			mul <= OC;
-		else 
-			mul <= QD;
+		if(count != 4'b1100) begin
+			if(count == 2 || count == 4 || count == 8 || count == 10)
+				mul = HX;
+			else if(count == 3 || count == 5 || count == 7 || count == 9)
+				mul = OC;
+			else 
+				mul = QD;
 
-		if(count == 0 ||  count == 1 || count == 2) row = 0;
-		else if(count == 3 || count == 4 || count == 5) row = 1;
-		else row = 2;
-
-		if(count == 0 || count == 3 || count == 6) col = 0;
-		else if(count == 1 || count == 4 || count  == 7) col = 1;
-		else col = 2;
-
-		if(r_ptr + 512 * row + col > 1026)
-			r_ptr_temp = r_ptr + 512* row + col - 1027;
-		else
-			r_ptr_temp = r_ptr + 512 * row + col;
+			if(count == 1 ||  count == 2 || count == 3) row = 0;
+			else if(count == 4 || count == 5 || count == 6) row = 1;
+			else row = 2;
+	
+			if(count == 1 || count == 4 || count == 7) col = 0;
+			else if(count == 2 || count == 5 || count == 8) col = 1;
+			else col = 2;
+	
+			if(r_ptr + 512 * row + col > 1026)
+				r_ptr_temp = r_ptr + 512* row + col - 1027;
+			else
+				r_ptr_temp = r_ptr + 512 * row + col;
 		end
 	end
 
@@ -85,7 +87,8 @@ module con_test_lb(rst, clk, din, i_en, result, done);
 			state <= 1'b0;
 			w_state <= 1'b0;
 			result <= 1'b0;
-		
+			f_cnt <= 1'b0;		
+
 			sum <= 1'b0;
 			sum_flip <= 1'b0;
 			temp_flip <= 1'b0;
@@ -96,8 +99,8 @@ module con_test_lb(rst, clk, din, i_en, result, done);
 
 			r_cnt <= 1'b0;
 			t_cnt <= 1'b0;
-			
 			done <= 1'b0;
+			o_en <= 1'b0;
 		end
 
 		else begin
@@ -113,7 +116,7 @@ module con_test_lb(rst, clk, din, i_en, result, done);
 				if(w_state == 1'b1) begin
 					if(w_ptr == 1026) begin
 						w_ptr <= 0;
-						state <= 1'b1;
+						if(state == 1'b0) state <= 1'b1;
 					end
 					else w_ptr <= w_ptr + 1;
 				end
@@ -123,28 +126,34 @@ module con_test_lb(rst, clk, din, i_en, result, done);
 				
 
 
-			if(state == 0) count <= 0;
-				
-			else begin
-				if(t_cnt == 509 && r_cnt == 509 && count == 4'b1010) begin
+			if(state == 0 || f_cnt != 'b0) count <= 0;
+			
+			if(state == 1 && f_cnt != 'b0) begin
+				count <= 0;
+				if(w_state == 1'b1) f_cnt <= f_cnt - 1;	
+			end
+
+			else if(state == 1 && f_cnt == 'b0) begin
+				if(t_cnt == 509 && r_cnt == 509 && count == 4'b1011) begin
 					state <= 1'b0;
 					done <= 1'b1;
 				end
 			
 				else begin
 					done <= 1'b0;	
-					if(count == 4'b1010) begin
+					if(count == 4'b1011) begin
 						if(i_en == 1'b1) count <= 4'b0000;
-						else count <= 4'b1011;
+						else count <= 4'b1100;
 					end
-					else if(count != 4'b1011)  count <= count + 1;
+					else if(count != 4'b1100)  count <= count + 1;
 				
-					if(count == 4'b1010) begin
+					if(count == 4'b1011) begin
 						if(r_cnt == 509) begin
 							if(r_ptr + 3 > 1026) r_ptr <= r_ptr + 3 - 1027;
 							else r_ptr <= r_ptr + 3;
 							r_cnt <= 0;
 							t_cnt <= t_cnt + 1;
+							f_cnt <= 3'b011;
 						end
 						else begin
 							if(r_ptr +1 > 1026) r_ptr <= r_ptr + 1 - 1027;
@@ -155,9 +164,11 @@ module con_test_lb(rst, clk, din, i_en, result, done);
 				end
 			end
 		
-			if(o_r_en == 1'b1) begin
+			if(c_r_en == 1'b1) begin
 				result <= temp_flip;
-			end 
+				o_en <= 1'b1;
+			end
+			else o_en <= 1'b0;
 
 			sum_flip <= temp_flip;
 		end	
--- con_test_lb_bench.v
+++ con_test_lb_bench.v
@@ -3,20 +3,24 @@ module top;
 	reg i_en;
 	reg[15:0] din;
 	wire done;
+	wire o_en;
 	wire [19:0] result;
 
-	reg[18:0] ad;
-	reg[15:0] data[0:262144];
-	reg[4:0] cycle;
+	reg[18:0] r_ad;
+	reg[15:0] r_data[0:262144];
+	reg[18:0] w_ad;
+	reg[15:0] w_data[0:260100];
+	reg[5:0] cycle;
 
 
 initial begin
 	clk = 1'b0;
 	rst = 1'b0;
 	i_en = 1'b0;
-	ad = 1'b0;
-	cycle = 4'b0000;
-	$readmemh("img_hex.dat", data);
+	r_ad = 1'b0;
+	w_ad = 1'b0;
+	cycle = 5'b00000;
+	$readmemh("img_hex.dat", r_data);
 	#3; rst = 1'b1;
 	#27; rst = 1'b0;
 end
@@ -33,13 +37,14 @@ end
 
 always@(posedge clk) begin
 	if(rst == 1'b1) begin
-		cycle <= 4'b0000;
-		ad <= 1'b0;
+		cycle <= 5'b00000;
+		r_ad <= 1'b0;
+		w_ad <= 1'b0;
 	end
 	else begin
-		if(cycle == 4'b1111) begin
-			cycle <= 4'b0000;
-			if(ad < 262144) ad <= ad + 1;
+		if(cycle == 5'b11000) begin
+			cycle <= 5'b00000;
+			if(r_ad < 262144) r_ad <= r_ad + 1;
 			i_en <= 1'b1;
 		end
 		else begin
@@ -48,11 +53,17 @@ always@(posedge clk) begin
 		end 
 	end
 
-	din <= data[ad];
+	din <= r_data[r_ad];
+	
+	if(o_en == 1'b1) begin
+		w_data[w_ad] <= result;
+		w_ad <= w_ad + 1;
+	end
 
 	if(done == 1'b1) begin
+		$writememh("img2_lb.dat", w_data);
 		$finish;
 	end
 end
-con_test_lb u0(rst, clk, din, i_en, result, done);
+con_test_lb u0(rst, clk, din, i_en, o_en, result, done);
 endmodule
