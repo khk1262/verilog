392aeed25b9fbf7376f033b126cbd3f7adf6512d
khk1262 <khk1262@naver.com>
2021-07-05 21:33:07 +0900

    last update

--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -1,12 +1,12 @@
 PIC_LD=ld
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _5434_archive_1.so
-_5434_archive_1.so : archive.264/_5434_archive_1.a
+ARCHIVE_OBJS += _16967_archive_1.so
+_16967_archive_1.so : archive.296/_16967_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared    -o .//../simv.daidir//_5434_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared    -o .//../simv.daidir//_16967_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_5434_archive_1.so $@
+	@ln -sf .//../simv.daidir//_16967_archive_1.so $@
 
 
 
